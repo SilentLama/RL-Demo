b9fc14c5ec71576f0418fc6c6a6577b58db9a7ab
unknown <flashthemusic@gmx.de>
2023-01-16 17:49:21 +0100

    Update examples

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ from app.rf.obstacle_course import ObstacleEnvironment, Obstacle
 from app.rf.model import MazeModel, DynaQPlusMazeModel, Model
 from app.rf.agents import DynaQAgent, AgentVisualizer, DynaQPlusAgent, ObstacleEnvironmentAgent
 from threading import Thread
-from tqdm import trange
+import numpy as np
+from tqdm import tqdm, trange
 
 if __name__ == "__main__":
+    ##### SINGLE AGENT SCENE #####
     # DynaQMazeScene(1920, 1080, "DynaQ-Demo", Maze.load_from_numpy_array("./mazes/first_example_maze_positive_reward.npy", (2, 0))).run()
-    # DynaQMazeScene(1920, 1080, "DynaQ-Demo", MazeGenerator.generate(80, 100, goal_reward=1)).run()
+
+    ##### SINGLE AGENT HARDER MAZE SCENE #####
+    # DynaQMazeScene(1920, 1080, "DynaQ-Demo", MazeGenerator.generate(10, 15, goal_reward=1)).run()
+
+    ##### MULTI AGENT SCENE #####
     # DynaQMazeMultiAgentScene(1920, 1080, "DynaQ-Multi-Agent-Demo", Maze.load_from_numpy_array("./mazes/blocking_maze_example.npy", (5, 3))).run()    
+
+    ##### MULTI AGENT DYNAQ+ SCENE #####
     # DynaQPlusMazeMultiAgentScene(1920, 1080, "DynaQ+-Multi-Agent-Demo", Maze.load_from_numpy_array("./mazes/blocking_maze_example.npy", (5, 3))).run()
 
 
-
-    # maze = MazeGenerator.generate(20, 30, algorithm="prims", goal_reward=1)
+    ##### PRIORITY SWEEP SCENE #####
+    # maze = MazeGenerator.generate(20, 30, algorithm="binary_tree", goal_reward=1)
     # maze_model = MazeModel(maze) # just there for compatibility
     # dynaq_plus_maze_model = DynaQPlusMazeModel(maze)
     # learning_rate = 0.1
@@ -34,9 +42,12 @@ if __name__ == "__main__":
     # for thread in train_threads:
     #     thread.join()
     # print(agent_one.step, agent_two.step, agent_three.step)
-    # MixedAgentsScene(1920, 1080, "DynaQ+-Multi-Agent-Demo", maze, (agent_one, agent_two, agent_three)).run()
+    # MixedAgentsScene(1920, 1080, "DynaQ+-PS-DynaQ-Agent-Demo", maze, (agent_one, agent_two, agent_three)).run()
 
-    obstacles = [Obstacle(1, 1, 2, 2), Obstacle(10, 4, 5, 3), Obstacle(2, 6, 2, 2), Obstacle(17, 17, 2, 2), Obstacle(4, 14, 1, 5)]
+
+    ##### OBSTACLE COURSE PRIORITY SWEEP SCENE #####
+
+    obstacles = [Obstacle(1, 1, 2, 2), Obstacle(10, 4, 5, 3), Obstacle(2, 6, 2, 2), Obstacle(17, 17, 2, 2), Obstacle(4, 14, 1, 5), Obstacle(14, 9, 1, 7)]
     obstacle_environment = ObstacleEnvironment(20, 20, obstacles, (12, 3, 80), [(5, 16, 100)])
     model = Model(obstacle_environment) # just there for compatibility
     learning_rate = 0.1
@@ -47,15 +58,13 @@ if __name__ == "__main__":
     pause = None
     agent_one = ObstacleEnvironmentAgent(obstacle_environment, model, learning_rate, discount_factor, epsilon, max_steps_per_episode, planning_steps, 5, pause = pause)        
     agent_two = ObstacleEnvironmentAgent(obstacle_environment, model, learning_rate, discount_factor, epsilon, max_steps_per_episode, planning_steps, 5, pause = pause, use_prioritized_sweeping=True, theta=0.000001)        
-    train_threads = []
-    def train(agent, n):
-        for _ in range(n):
+            
+    for agent in tqdm((agent_one, agent_two)):
+        for _ in trange(20):
             agent.train_episode()
-    for agent in (agent_one, agent_two):
-        t = Thread(target = train, args = [agent, 10], daemon=True)
-        train_threads.append(t)
-        t.start()
-    for thread in train_threads:
-        thread.join()
+
+    for i, agent in enumerate((agent_one, agent_two)):
+        np.save(f"agent_{i}_value_function.npy", agent.value_function)
+        np.save(f"agent_{i}_value_function.npy", agent.visited_state_actions)
     print(agent_one.step, agent_two.step)
     ObstacleCourseScene(1920, 1080, "Obstacle-Course-Demo", obstacle_environment, (agent_one, agent_two)).run()
